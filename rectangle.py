5260821d4fc4518307e559ca34a0e7b3c92184f1
frey-norden <frey.norden@icloud.com>
2020-05-30 13:02:42 +0300

    simple simple rectangle

---
+++ rectangle.py
@@ -0,0 +1,8 @@
+width = int(input('Width? '))
+height = int(input('Height? '))
+
+def rectangle_area(width, height):
+    area = width * height
+    print('The area is ' + str(area))
+
+rectangle_area(width, height)
