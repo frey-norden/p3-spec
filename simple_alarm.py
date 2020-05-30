123679b2fbd6773f1ef882dfffd460200158ed15
frey-norden <frey.norden@icloud.com>
2020-05-30 11:19:04 +0300

    fixed same day bug

--- simple_alarm.py
+++ simple_alarm.py
@@ -8,6 +8,8 @@ def alarm():
     alarm = int(input('Enter number of hours: '))
     current_time = int(input('Enter current hour: '))
     new_time = (alarm % 24) + current_time
+    if new_time > 24:
+        new_time -= 24
     return new_time
 
 def main():
