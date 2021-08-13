ea517ed1670266a829f6e43d061e3bfb7d6ae463
Jeffrey Jackson <jeffrey.jackson@tuni.fi>
2021-08-13 23:36:21 +0300

    new sorting script

---
+++ sort_d_keys.py
@@ -0,0 +1,13 @@
+L = ['E', 'F', 'B', 'M', 'C', 'X', 'X', 'X', 'A', 'D', 'I', 'I', 'C', 'B', 'A', 'D', 'D', 'E', 'D']
+
+d = {}
+for x in L:
+    if x in d:
+        d[x] += 1
+    else:
+        d[x] = 1
+
+y = sorted(d.keys())
+
+for k in y:
+    print('{} appears {} times in this list'.format(k, d[k]))    
